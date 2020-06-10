e8e0bada5c71c33f523148312da0541f245d93fe
dohyeonkim2526 <65331451+dohyeonkim2526@users.noreply.github.com>
2020-06-11 00:41:47 +0900

    array and pointer

--- exercise1_17101937김도현.c
+++ exercise1_17101937김도현.c
@@ -1,36 +1,59 @@
-#pragma warning(disable:4996)
 #include<stdio.h>
-#define stop '#'
+#define s 5
 
-void chline(char, unsigned int, unsigned int);
+void copy_arr(double array1[], double array2[], unsigned int n);
+void copy_ptr(double *a, double *b, unsigned int n);
+void copy_ptrs(double *a, double *b, double *n);
 
 int main(void)
 {
-	char ch;
-	int i, j;
-
-	printf("Enter a char and two ints(# to quit): ");
-
-	while ((scanf("%c %d %d", &ch, &i, &j) == 3) && (getchar != stop))
-	{
-		chline(ch, i, j);
-		printf("\n");
-
-		printf("Enter a char and two ints(# to quit): ");
-	}
+	double source[5] = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+	double target1[5];
+	double target2[5];
+	double target3[5];
+	copy_arr(target1, source, 5);
+	copy_ptr(target2, source, 5);
+	copy_ptrs(target3, source, source + 5);
 
 	return 0;
 }
 
-void chline(char ch, unsigned int i, unsigned int j)
-{
-	int columns;
+void copy_arr(double array1[], double array2[], unsigned int n) {
+	
+	int index;
 
-	for (columns = 1; columns < i; columns ++)
-		putchar(' ');
+	for (index = 0; index < n; index++)
+		array1[index] = array2[index];
 
-	for (; columns <= j; columns ++)
-		putchar(ch);
+	for (index = 0; index < n; index++)
+		printf("target1[%d]=%.2lf\n", index, array1[index]);
+	
+
 }
 
+void copy_ptr(double *a, double *b, unsigned int n) {
 
+	int index;
+
+	for (index = 0; index < n; index++)
+		*(a + index) = *(b + index);
+
+	for (index = 0; index < n; index++)
+		printf("target2[%d]=%.2lf\n", index, a[index]);
+
+
+}
+
+void copy_ptrs(double *a, double *b, double *n) {
+	
+	int index;
+	double* target = a;
+	while (b < n) {
+		*target = *b;
+		b++;
+		target++;
+	}
+
+	for (index = 0; index < s; index++)
+		printf("target3[%d]=%.2lf\n", index, *(a+index));
+}
